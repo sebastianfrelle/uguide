99e0c820a74c9967f54b7bb92940a8c2fb4bee53
Hmoulvad <Hmoulvad@hotmail.com>
2018-04-22 15:04:50 +0200

    Addede constraints to Login and show course

--- uguide/AuthViewController.swift
+++ uguide/AuthViewController.swift
@@ -1,17 +1,30 @@
 import UIKit
 
 class AuthViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBAction func loginBtn(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+
+    
+    func getUserInfo () {
+        let username = usernameTextField.text
+        let password = usernameTextField.text
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
