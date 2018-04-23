1a4bc194ed8aa23bbbc0ef49cfdbc4a88c89a1f8
Hmoulvad <Hmoulvad@hotmail.com>
2018-04-23 14:03:07 +0200

    Minor things

--- uguide/AuthViewController.swift
+++ uguide/AuthViewController.swift
@@ -1,9 +1,10 @@
 import UIKit
 
 class AuthViewController: UIViewController {
-
-    
-    @IBOutlet var UILabel: [UILabel]!
+        
+    @IBOutlet var Labels: [UILabel]!
+    @IBOutlet var TextFields: [UITextField]!
+    @IBOutlet weak var LoginBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,6 +16,12 @@ class AuthViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func setUpUserInterface() {
+        UILabel.appearance().font = UIFont(name: "Lato-Regular", size: 16)
+        UITextView.appearance().font = UIFont(name: "Lato-Regular", size: 14)
+        LoginBtn.layer.cornerRadius = 6
     }
     
 }
